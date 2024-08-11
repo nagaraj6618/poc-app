fa1902ada742bf78b9b56055d845a6515d4a1bdd
NAGARAJ S <nagaraj516700@gmail.com>
2024-08-11 15:50:41 +0530

    Added terraform and checking -02

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,24 @@
-resource "local_file" "copy_build" {
-  for_each = fileset(var.build_source_path, "**/*")
-
-  source      = "${var.build_source_path}/${each.value}"
-  destination = "${var.deploy_destination_path}/${each.value}"
-}
-
 resource "null_resource" "create_deploy_directory" {
   provisioner "local-exec" {
-    command = "mkdir -p ${var.deploy_destination_path}"
+    command = "powershell -Command \"New-Item -ItemType Directory -Force -Path '${var.deploy_destination_path}'\""
   }
 
   triggers = {
     always_run = "${timestamp()}"
   }
 }
+
+resource "null_resource" "copy_build_files" {
+  depends_on = [null_resource.create_deploy_directory]
+
+  provisioner "local-exec" {
+    command = <<EOT
+powershell -Command "xcopy '${replace(var.build_source_path, "\\\\$", "")}' '${replace(var.deploy_destination_path, "\\\\$", "")}' /E /I /Y"
+EOT
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
