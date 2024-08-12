3fd358bc0a50a547e90df0931cf12f67d300178e
NAGARAJ S <nagaraj516700@gmail.com>
2024-08-12 16:40:54 +0530

    Install and build via terraform checking - 12

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,14 @@ resource "null_resource" "copy_build_files" {
   }
   depends_on = [null_resource.npm_build]
 }
+
+
+resource "local_file" "copy_build_files" {
+  content = <<-EOT
+    npm install && npm run build && xcopy ${var.build_source_path} ${var.deploy_destination_path} /E /I /Y
+  EOT
+  filename = "${path.module}/copy_file.bat"
+  file_permission = "0755"
+}
+
+
