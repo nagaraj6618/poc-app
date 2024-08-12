a55cd0916d0a88b06b2e484c7f48ac15f470da08
NAGARAJ S <nagaraj516700@gmail.com>
2024-08-12 16:55:06 +0530

    terraform checking - 14

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Welcome to my websites</h1>
+      <h1>Welcome to My Poc </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
