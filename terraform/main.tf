ea1cf574f8544028ed665b8ef74a4442129c37c1
nagaraj6618 <nagaraj516700@gmail.com>
2024-09-04 17:16:21 +0530

    Updated Terraform - check 05

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,9 @@ resource "null_resource" "run_sh_file" {
 
   provisioner "local-exec" {
     command = <<EOF
-      ssh-keyscan -H localhost >> ~/.ssh/known_hosts
       npm install
       npm run build
-      whoami
-      ls
-      ssh -o StrictHostKeyChecking=no localhost sudo cp -rf /var/lib/jenkins/workspace/poc-app/build/* /var/www/html
+      sudo cp -rf ../build/* /var/www/html
       echo "Finished"
 EOF
   }
